54dadecb7acc2e4e3e4d993a13ca3c82bbd67fb0
Ze Zan Ziet <vietvan05032002@gmail.com>
2024-05-28 15:05:13 +0700

    Upload spring boot ecall-backend project.

--- src/main/java/vn/ecall/S3/AmazonS3Configuration.java
+++ src/main/java/vn/ecall/S3/AmazonS3Configuration.java
@@ -10,14 +10,12 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class AmazonS3Configuration {
 
-    /*@Bean
+    @Bean
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.AP_SOUTHEAST_2)
                 .credentialsProvider(StaticCredentialsProvider.
-                        create(AwsBasicCredentials.create(
-                        "",
-                        "")))
+                        create())
                 .build();
-    }*/
+    }
 }
